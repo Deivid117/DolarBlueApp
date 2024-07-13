635f20a5d10d05a334cb9bc0814754cd648ff990
deivid <oni_grt56@hotmail.com>
2024-07-13 11:06:08 -0600

    FEATURE - History UI
    - Diseño de la interfaz

--- app/src/main/java/com/varqulabs/dolarblue/history/presentation/HistoryScreen.kt
+++ app/src/main/java/com/varqulabs/dolarblue/history/presentation/HistoryScreen.kt
@@ -1,36 +1,161 @@
 package com.varqulabs.dolarblue.history.presentation
 
+import android.graphics.Rect
+import android.view.View
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.composed
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.varqulabs.dolarblue.R
 import com.varqulabs.dolarblue.core.presentation.generics.top_bars.DrawerAppBar
-import com.varqulabs.dolarblue.history.presentation.HistoryEvent.OnClickDrawer
+import com.varqulabs.dolarblue.history.domain.model.Conversion
+import com.varqulabs.dolarblue.history.presentation.HistoryEvent.*
+import com.varqulabs.dolarblue.history.presentation.components.ButtonToScrollToTop
+import com.varqulabs.dolarblue.history.presentation.components.ConversionHistoryContent
+import com.varqulabs.dolarblue.history.presentation.components.HistorySearchCard
+import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
+import kotlinx.datetime.toLocalDateTime
 
 @Composable
 fun HistoryScreen(
     state: HistoryState,
     eventHandler: (HistoryEvent) -> Unit
 ) {
+    LaunchedEffect(Unit) { eventHandler(Init) }
+    /*LaunchedEffect(state.reload) { if (state.reload) eventHandler(HistoryEvent.Init) }*/
+
+    val lazyListState = rememberLazyListState()
+
     Scaffold(
         topBar = {
             DrawerAppBar(
-                title = stringResource(id = R.string.copy_history),
-                actionIcon = R.drawable.favoritos_1_p,
-                onClickDrawer = { eventHandler(OnClickDrawer) }
+                title = "",
+                actionIcon = R.drawable.favoritos_1_n,
+                onClickDrawer = { eventHandler(OnClickDrawer) },
+                onClickActionIcon = { eventHandler(OnClickGetFavoriteConversions) }
+            )
+        },
+        floatingActionButton = {
+            ButtonToScrollToTop(
+                lazyListState = lazyListState,
+                listIsNotEmpty = state.conversionsHistory.isNotEmpty()
             )
         }
-    ) {
+    ) { paddingValues ->
         Column(
             modifier = Modifier
-                .padding(it)
-                .fillMaxSize()
+                .padding(paddingValues)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
+            HistorySearchCard(
+                state = state,
+                eventHandler = eventHandler
+            )
 
+            ConversionHistoryContent(
+                lazyListState = lazyListState,
+                state = state,
+                eventHandler = eventHandler
+            )
         }
     }
+}
+
+// TODO: Función para manejar el gesto de back para hacer alguna acción
+@Composable
+public fun BackHandler(enabled: Boolean = true, onBack: () -> Unit) {
+    // Safely update the current `onBack` lambda when a new one is provided
+    val currentOnBack by rememberUpdatedState(onBack)
+    // Remember in Composition a back callback that calls the `onBack` lambda
+    val backCallback = remember {
+        object : OnBackPressedCallback(enabled) {
+            override fun handleOnBackPressed() {
+                currentOnBack()
+            }
+        }
+    }
+    // On every successful composition, update the callback with the `enabled` value
+    SideEffect {
+        backCallback.isEnabled = enabled
+    }
+    val backDispatcher = checkNotNull(LocalOnBackPressedDispatcherOwner.current) {
+        "No OnBackPressedDispatcherOwner was provided via LocalOnBackPressedDispatcherOwner"
+    }.onBackPressedDispatcher
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner, backDispatcher) {
+        // Add callback to the backDispatcher
+        backDispatcher.addCallback(lifecycleOwner, backCallback)
+        // When the effect leaves the Composition, remove the callback
+        onDispose {
+            backCallback.remove()
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewScreen() {
+    HistoryScreen(
+        state = HistoryState(),
+        eventHandler = {}
+    )
 }
